962e1968aba59d2b61d60e40d851fc02818c9210
Wojciech Żyła <wojciechzyla@MacBook-Pro-Wojciech.local>
2020-04-20 21:34:33 +0200

    dodanie funkcji ładującej dane z pliku

---
+++ main.py
@@ -0,0 +1,32 @@
+
+
+class Contact:
+    def __init__(self, name, surname, email):
+        self.name = name
+        self.surname = surname
+        self.email = email
+
+
+def loadContacts():
+    contacts = []
+
+    try:
+        with open('ksiazka.txt') as f:
+            for x in f:
+                arr = x.split()
+                name = arr[0]
+                surname = arr[1]
+                email = arr[2]
+                contacts.append(Contact(name, surname, email))
+    except IOError:
+        f = open('ksiazka.txt','x')
+        f.close()
+
+    return contacts
+
+
+def main():
+    loadContacts()
+    return 0
+
+main()
