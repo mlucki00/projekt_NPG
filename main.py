09da3678c90f8f86f6e1644c5bcbd5fa8be272a9
wojciechzyla <wojciechzyla.mail@gmail.com>
2020-06-06 16:31:08 +0200

    Dodanie funkcji sendmail()

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-
+import smtplib, ssl, time
 
 class Contact:
-    def __init__(self, name, surname, email):
+    def __init__(self, name, surname, email, group):
         self.name = name
         self.surname = surname
         self.email = email
+        self.group = group
 
 def alphabeticalorder(obj):
     result :str = obj.name+obj.surname
@@ -41,7 +42,27 @@ def closeprogram(contacts):
     except IOError:
         print("nie moglem zapisac danych")
 
+def sendmail(mail):
+    port = 465  # For SSL
+    smtp_server = "smtp.gmail.com"
+    sender_email = "projeknpg@gmail.com"
+    receiver_email = mail
+    password = "zaliczenie_projektu2020"
+    print("podaj temat maila")
+    subject = input()
+    print("podaj tresc maila")
+    content = input()
+    message = """\
+    {}
 
+    {}""".format(subject, content)
+
+    print("Trwa wysylanie...")
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+    print("Wyslano")
 
 def main():
 
